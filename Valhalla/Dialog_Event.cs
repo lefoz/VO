1c53ae746e16f6044e082478440ec71e1059420f
lefoz <lefoz@LEFOZ>
2017-02-07 16:16:23 +0100

    second

--- Valhalla/Dialog_Event.cs
+++ Valhalla/Dialog_Event.cs
@@ -13,7 +13,7 @@ using Android.Widget;
 namespace Valhalla
 {
     class Dialog_Event : Android.Support.V4.App.DialogFragment
-    {
+    { //test
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
